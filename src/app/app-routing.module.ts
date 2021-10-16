d8f42a83829b46d2ec4075b586158a759ced892f
John Doe <interglare@gmail.com>
2021-10-16 23:40:43 +0600

    practice2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PageComponent } from './page/page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: PageComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
